04f20fa26b13af7fc0cd69c0e35c082ead6ce38b
Katherine Winters <goble.katherine@gmail.com>
2022-02-08 09:41:26 -0600

    initial commit

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -8,15 +8,10 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldDoSomething()
         {
-            // TODO: Complete Something, if anything
-
-            //Arrange
             var tacoParser = new TacoParser();
 
-            //Act
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
-            //Assert
             Assert.NotNull(actual);
 
         }
@@ -25,20 +20,12 @@ namespace LoggingKata.Test
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete - "line" represents input data we will Parse to
-            //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
 
-            //Arrange
             var tacoTest = new TacoParser();
-            //Act
+            
             var actual = tacoTest.Parse(line);
-            //Assert
+            
             Assert.Equal(expected, actual.Location.Longitude);
         }
-
-
-        //TODO: Create a test ShouldParseLatitude
-
     }
 }
--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -13,43 +13,19 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-            // TODO:  Find the two Taco Bells that are the furthest from one another.
-            // HINT:  You'll need two nested forloops ---------------------------
-
             logger.LogInfo("Log initialized");
-
-            // use File.ReadAllLines(path) to grab all the lines from your csv file
-            // Log and error if you get 0 lines and a warning if you get 1 line
 
             var lines = File.ReadAllLines(csvPath);
 
             logger.LogInfo($"Lines: {lines[0]}");
 
-
-            // Create a new instance of your TacoParser class
-
             var parser = new TacoParser();
 
-            // Grab an IEnumerable of allRestaurants using the Select command: var allRestaurants = lines.Select(parser.Parse);
-
             var allRestaurants = lines.Select(parser.Parse).ToArray();
-
-            // DON'T FORGET TO LOG YOUR STEPS
-
-            // Now that your Parse method is completed, START BELOW ----------
-
-            // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
-            // Create a `double` variable to store the distance
 
             ITrackable finalRestaurant1 = null;
             ITrackable finalRestaurant2 = null;
             double finalDistance = 0;
-
-
-            // Include the Geolocation toolbox, so you can compare allRestaurants: `using GeoCoordinatePortable;`
-
-            //HINT NESTED LOOPS SECTION---------------------
-            // Do a loop for your allRestaurants to grab each location as the origin (perhaps: `locA`)
 
             ITrackable locA = null;
             ITrackable locB = null;
@@ -84,21 +60,6 @@ namespace LoggingKata
             double? v = finalDistance / num;
             Console.WriteLine();
             Console.WriteLine($"The distance between {finalRestaurant1.Name} and {finalRestaurant2.Name} is {finalDistance} miles apart.");
-
-
-            // Create a new corA Coordinate with your locA's lat and long
-
-            // Now, do another loop on the allRestaurants with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
-
-            // Create a new Coordinate with your locB's lat and long
-
-            // Now, compare the two using `.GetDistanceTo()`, which returns a double
-            // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
-
-            // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-
-
-
         }
     }
 }
